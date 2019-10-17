810168bf806e67fdc0db7cf163b72e03c6ece6c0
sanya-shch <shchipkov.sanya@ukr.net>
2019-10-17 22:08:04 +0300

    burger menu button - arrow

---
+++ Burger Menu Button/index.js
@@ -0,0 +1,6 @@
+document.querySelectorAll('.menu-btn').forEach(t=>t.addEventListener('click', ()=> t.classList.toggle('menu-btn_active') , false));
+
+// $('.menu-btn').on('click', function(e) {
+//   e.preventDefault;
+//   $(this).toggleClass('menu-btn_active');
+// });
