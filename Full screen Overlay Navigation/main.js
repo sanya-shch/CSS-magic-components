80f2fa3e896d6481430acccc43bcc937470ed518
sanya-shch <shchipkov.sanya@ukr.net>
2020-02-05 23:55:28 +0200

    Full screen Overlay Navigation

---
+++ Full screen Overlay Navigation/main.js
@@ -0,0 +1,6 @@
+function menuToggle() {
+    const nav = document.getElementById('nav');
+    const toggle = document.getElementById('toggle');
+    nav.classList.toggle('active');
+    toggle.classList.toggle('active');
+}
